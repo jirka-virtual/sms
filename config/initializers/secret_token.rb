aeb99eaa463fe57314d66aeb8b2e77ef9d107d3c
Jiri <jiri@jiriprochazka.com>
2014-04-03 21:27:57 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sms::Application.config.secret_key_base = '32ce75e9d85f1355c467d3e7e70ae74b5a1b97c6a0d77bf3bb3d800ec984370eeda841bb8d45ae9a98ccfb44b71565a6f42cdf8ec449598e8206ca5a4fe7f023'
